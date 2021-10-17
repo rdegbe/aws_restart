79be72dc73cb9866002097893220a081e50e24e5
raymond degbe <rdegbe14@gmail.com>
2021-10-17 07:19:36 +0000

    Added all lost files

---
+++ collections.py
@@ -0,0 +1,31 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+myFruitList[2] = "orange"
+print(myFruitList)
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+myFavoriteFruitDictionary = {
+    "Akua" : "apple",
+    "Saana" : "banana",
+    "Paulo" : "pineapple"
+}
+
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saana"])
+print(myFavoriteFruitDictionary["Paulo"])
